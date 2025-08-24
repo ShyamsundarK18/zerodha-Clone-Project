116fe4205f5dbe9ed5806455590eb39cf8a719a3
Shyam-7865 <engr.shyamkumar7@gmail.com>
2025-08-24 16:11:24 +0530

    Add dashboard folder as regular directory

--- dashboard/src/components/home/Home.jsx
+++ dashboard/src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import Dashboard from "../Dashboard/Dashboard.jsx";
+import Dashboard from "../dashboard/Dashboard.jsx";
+// import Dashboard from "../Dashboard/Dashboard.jsx";
 import TopBar from "../topbar/TopBar.jsx";
 
 function Home() {
